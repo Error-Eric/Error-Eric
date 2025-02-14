b319c65f6eb03bd42366c369a86eacbd7064aaa3
Eric <zhouchanghuieric@qq.com>
2025-02-14 14:21:52 +0800

    minor update in whk and now

--- now/my.cpp
+++ now/my.cpp
@@ -2,111 +2,47 @@
 #include<algorithm>
 #include<vector>
 #include<numeric>
-#include<map>
-#include<queue>
-#include<unordered_set>
 //#include<ranges>
 using namespace std;
-int q, x, y, z;
-#define LS (o<<1)
-#define RS (o<<1|1)
-class segt{
-	private:
-	vector<int> dat, tree;
-	map<int, queue<int> > nex;
-	int lent = 2e5+5;
-	segt(){
-		dat.resize(lent + 5),
-		tree.resize((lent << 1) + 15),
-		fill(dat.begin(), dat.end(), 2.1e6),
-		iota(tree.begin(), tree.end(), 0);
-	}
-	void chg(int o, int sl, int sr, int&qo, int&v){
-		if(sl == qo && sr == qo) return (void)(dat[o] = v);
-		else if(qo < sl || sr < qo) return ;
-		else {
-			int mid = ((sl + sr) >> 1);
-			if(qo < mid) chg(LS, sl, mid, qo, v);
-			else chg(RS, mid+1, sr, qo, v);
-			tree[o] = (dat[tree[LS]]< dat[tree[RS]])? tree[LS] : tree[RS];
-		}
-	}
-	int query(int o, int sl, int sr, int ql, int qr){
-		if(ql <= sl && sr <= qr)return dat[o];
-		else if(qr < sl || sr < ql) return lent;
-		else {
-			int mid = ((sl + sr) >> 1), 
-			al = query(LS, sl, mid, ql, qr),
-			ar = query(RS, mid+1, sr, ql, qr);
-			return (dat[al] < dat[ar]) ? al : ar; 
-		}
-	}
-	public:
-	int query(int ql, int qr){return query(1, 1, lent, ql, qr);}
-	void modify(int pos, int val){
-		if(dat[pos]<2.05e6) nex[pos].push(val);
-		else chg(1, 1, lent, pos, val);
-	}
-	pair<int, int> leave(int maxppl){
-		int id = query(1, 1, lent, 0, maxppl), ppl = dat[id];
-		if(nex.count(ppl)){
-			int nexin = nex[ppl].front();
-			nex[ppl].pop();
-			if(nex[ppl].empty()) nex.erase(ppl); 
-			chg(1, 1, lent, ppl, nexin);
-		}
-		else chg(1, 1, lent, ppl, lent);
-		return {id, ppl};
-	}
-}t;
-
-struct divgoup{
-	mutable int pop;
-	int id;
-};
-queue<divgoup> q2;
-unordered_set<int> lefta;
-
+typedef pair<int, int> pii;
+int n, c, x, tot;
+vector<int> points, stk, ans, dest;
+vector<pii > pairs;
+#define LP pairs.back().first, pairs.back().second
 int main(){
 	ios::sync_with_stdio(0),
 	cin.tie(0), cout.tie(0);
-	t.modify(1, 4);
-	t.modify(1, 5);
-	t.modify(7, 8);
-	t.modify(2, 10);
-
-	return 0;
-	cin>> q;
-	while(q--){
-		cin >> x;
-		switch (x)
-		{
-		case 1: 
-			cin >> x >> y,(y == 1)? 
-			q2.push({x, ++z}): t.modify(x, ++z);
-			cout<<"1!" << endl;
-			break;
-		case 2: 
-			cin >> x;
-			while(x){
-				while(lefta.count(q2.front().id)) q2.pop();
-				while(lefta.count(t.query(1, x))) t.leave(x);
-				int best = t.query(1, x);
-				if(q2.empty() || best < q2.front().id) 
-					x -= t.leave(x).second;
-				else {
-					if(best > 2e5) break;
-					cout << q2.front().id << " " << min(x, q2.front().pop)<< endl;
-					if(x < q2.front().pop) q2.front().pop -= x, x = 0;
-					else x -= q2.front().pop, q2.pop();
-				}
-			}
-			break;
-			
-		default:
-			cin>> x;
-			lefta.insert(x);
-			break;
-		}
+	cin >> n >> c;
+	for(int i : {-1,1})
+		for(int j = 1; j <= n; j++)
+			cin >> x, points.push_back(i*x);
+	sort(points.begin(), points.end(), 
+		[&](int u,int v){return abs(u)<abs(v);});
+	for(int px : points){
+		if(stk.empty() || stk.back() * px > 0) stk.push_back(px);
+		else pairs.push_back({stk.back(), px}), stk.pop_back();
 	}
+	sort(pairs.begin(), pairs.end(), 
+		[&](pii u, pii v){return abs(u.first)> abs(v.first);});
+	//for(auto px : pairs)
+	//	cout << px.first << " " << px.second << endl;
+	while(pairs.size()){
+		dest.clear(), x= 0;
+		for(int i = 1; i <= c; i++){
+			if(x == 0){
+				tot += abs(pairs.back().first+pairs.back().second);
+			}
+			if(pairs.size() && pairs.back().first * x >= 0)
+				dest.push_back(max(LP)), 
+				ans.push_back(min(LP)),
+				x = pairs.back().first,
+				pairs.pop_back();
+			else break;
+		}
+		while(dest.size()) 
+			ans.push_back(-dest.back()), dest.pop_back();
+
+	}
+	cout << tot << endl;
+	for(int ax : ans) cout << -ax << " ";
 }
