2455a71e8ee961a80ff28e28e42bb0b48e2d54b3
Eric <zhouchanghuieric@qq.com>
2025-04-27 22:24:48 +0800

    minor update ee, modify modified labreport 6

--- ee/vector_set.h
+++ ee/vector_set.h
@@ -1,56 +1,94 @@
-#include"abstract_set.h"
+#include <vector>
+#include <algorithm>
 
-template <typename T, typename Cmp = std::less<T> >
-class VectorSet : public AbstractSet<T, Cmp> {
+template <typename T>
+class VectorSet {
 private:
-    std::vector<T> data;
-    Cmp cmp;
+    std::vector<T> elements;
 
 public:
-    // Concrete iterator implementation
-    class Iterator : public AbstractSet<T, Cmp>::iterator {
-        typename std::vector<T>::iterator vec_it;
-    public:
-        Iterator(typename std::vector<T>::iterator it) : vec_it(it) {}
-        T& operator*() const override { return *vec_it; }
-        Iterator& operator++() override { ++vec_it; return *this; }
-        bool operator!=(const typename AbstractSet<T, Cmp>::iterator& other) const override {
-            const Iterator* other_it = dynamic_cast<const Iterator*>(&other);
-            return (other_it == nullptr) || (vec_it != other_it->vec_it);
-        }
-    };
+    VectorSet() = default;
+    VectorSet(const std::vector<T>& vec){
+        elements = vec;
+    }
 
-    // Overridden virtual methods
-    void insert(const T& x) override {
-        if (std::find(data.begin(), data.end(), x) == data.end()) {
-            data.push_back(x);
+    // Inserts an element into the set if not already present
+    void insert(const T& value) {
+        auto it = std::lower_bound(elements.begin(), elements.end(), value);
+        if (it == elements.end() || *it != value) {
+            elements.insert(it, value);
         }
     }
 
-    void erase(const T& x) override {
-        auto it = std::find(data.begin(), data.end(), x);
-        if (it != data.end()) data.erase(it);
-    }
-
-    size_t count(const T& x) override { 
-        return (std::find(data.begin(), data.end(), x) != data.end()) ? 1 : 0;
-    }
-
-    size_t size() const noexcept override { return data.size(); }
-
-    // Return concrete iterators
-    Iterator begin() override { return Iterator(data.begin()); }
-    Iterator end() override { return Iterator(data.end()); }
-
-    // Other methods
-    void insert_set(const AbstractSet<T, Cmp>& friend_set) override {
-        for (auto it = friend_set.begin(); it != friend_set.end(); ++it) {
-            this->insert(*it);
+    // Removes an element from the set if present
+    void erase(const T& value) {
+        auto it = std::lower_bound(elements.begin(), elements.end(), value);
+        if (it != elements.end() && *it == value) {
+            elements.erase(it);
         }
     }
 
-    AbstractSet<T, Cmp>& operator+=(const AbstractSet<T, Cmp>& friend_set) override {
-        insert_set(friend_set);
+    // Checks if an element exists in the set
+    bool contains(const T& value) const {
+        return std::binary_search(elements.begin(), elements.end(), value);
+    }
+
+    // Returns the number of elements in the set
+    size_t size() const {
+        return elements.size();
+    }
+
+    // Checks if the set is empty
+    bool empty() const {
+        return elements.empty();
+    }
+
+    // Allows iteration over the elements (read-only)
+    typename std::vector<T>::const_iterator begin() const {
+        return elements.begin();
+    }
+
+    typename std::vector<T>::const_iterator end() const {
+        return elements.end();
+    }
+
+    // Optional: Clear all elements from the set
+    void clear() {
+        elements.clear();
+    }
+
+     // Merge another VectorSet into this one (union operation)
+     void merge(const VectorSet<T>& other) {
+        std::vector<T> merged;
+        merged.reserve(elements.size() + other.elements.size());
+        
+        auto it1 = elements.begin(), end1 = elements.end();
+        auto it2 = other.elements.begin(), end2 = other.elements.end();
+
+        while (it1 != end1 && it2 != end2) {
+            if (*it1 < *it2) {
+                merged.push_back(*it1);
+                ++it1;
+            } else if (*it2 < *it1) {
+                merged.push_back(*it2);
+                ++it2;
+            } else { // Equal elements
+                merged.push_back(*it1);
+                ++it1;
+                ++it2;
+            }
+        }
+
+        // Add remaining elements from either vector
+        merged.insert(merged.end(), it1, end1);
+        merged.insert(merged.end(), it2, end2);
+        
+        elements = std::move(merged);
+    }
+
+    // Merge-and-assign operator
+    VectorSet<T>& operator+=(const VectorSet<T>& other) {
+        merge(other);
         return *this;
     }
 };
